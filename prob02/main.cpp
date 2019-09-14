5d1df26d0b19ec1e79ce8a3904c756385bd33499
willyr2501 <willyr2501@cs104u-3a.acad.fullerton.edu>
2019-09-14 15:52:16 -0700

    Resubmit

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -6,15 +6,15 @@ int main()
 {
   // one gallon of paint covers 340 square feet
   double coverage = 340;
-   int height = 6;
-    int width = 100;
+  int height = 6;
+  int width = 100;
 
   // calculate how many gallons of paint it will take to cover
   // the area with 2 coats of paint
-  int area=height*width*2;
-    double gallons = area / coverage;
+  int area = height * width * 2;
+  double gallons = area / coverage;
 
-  std::cout << "It will take " << gallons << " gallons of paint./n";
+  std::cout << "It will take " << gallons << " gallons of paint" <<std::endl;
 
   return 0;
 }
